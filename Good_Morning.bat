4ce562aa0bbeed8a8aceb511fc3a2e1383d13cda
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-07-24 08:55:10 +0545

    Initial: Add News url

--- Good_Morning.bat
+++ Good_Morning.bat
@@ -1,1 +1,9 @@
-"Morning" 
+@echo off
+@echo off
+start "chrome.exe" "C:\Program Files\Google\Chrome\Application\chrome.exe" https://www.setopati.com/  https://www.androidpolice.com/ https://www.ratopati.com/ https://www.gadgetbytenepal.com/   https://ekantipur.com/ https://www.bbc.com/news/world https://www.theverge.com/  https://www.bizshala.com/ https://myrepublica.nagariknetwork.com/ https://www.bleepingcomputer.com/ https://merolagani.com/ https://beebom.com/ https://www.onlinekhabar.com/
+#start "firefox.exe" "C:\Program Files\Mozilla Firefox\firefox.exe" https://www.bizshala.com/ https://merolagani.com/ https://www.setopati.com/ https://www.onlinekhabar.com/ https://www.ratopati.com/ https://www.theverge.com/ https://www.xda-developers.com/ https://www.bleepingcomputer.com/ https://ekantipur.com/ https://www.bloomberg.com/asia https://news.google.com/topstories?hl=en-US&gl=US&ceid=US:en https://www.bbc.com/news/world https://www.maketecheasier.com/ https://www.pcmag.com/  https://www.businessinsider.com/sa https://www.androidpolice.com/ https://www.windowscentral.com/blog https://www.computerworld.com/in/category/windows/ https://www.linuxtoday.com/ https://thehackernews.com/ https://www.androidauthority.com/ https://www.gadgetbytenepal.com/ https://mspoweruser.com/ https://beebom.com/ https://www.cnbc.com/world/ https://awesomeopensource.com/ https://www.kitploit.com/ https://www.producthunt.com/
+#start  https://www.bizshala.com/
+#start https://merolagani.com/
+exit
+
+
