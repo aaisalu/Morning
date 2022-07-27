24e978dbc34d3a965b9a2226049ab5893ea9e556
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-07-27 07:10:36 +0545

    Browser: Add Brave Support

--- Good_Morning.bat
+++ Good_Morning.bat
@@ -5,9 +5,9 @@ echo "TIME IS PASSING & DON't WASTE it OR else You'll regret soon"
 
 echo[
 
-echo Press 1=="Darling it's News"
-echo Press 2=="Tech News"
-echo Press 3=="Stock"
+echo Press 1=="Start with day with Updated News Sources"
+echo Press 2=="It's Stock Time"
+echo Press 3=="Navigate to Project Folder."
 
 echo[
 echo Jst a little bit more ...
@@ -26,39 +26,82 @@ if /I "%c%" EQU "5" goto :jstfar
 goto :choice
 
 
-:somewhere 
+:somewhere
 
-echo "I am here because you want to see News"
-start "Firefox" "C:\Program Files\Mozilla Firefox\firefox.exe" https://www.bizshala.com/ https://merolagani.com/ https://www.cnbc.com/world/ https://www.setopati.com/ https://www.onlinekhabar.com/ https://www.ratopati.com/ https://ekantipur.com/ https://www.bloomberg.com/asia https://news.google.com/topstories?hl=en-US&gl=US&ceid=US:en https://www.cnbc.com/world/ https://t.me/WorldNews
+echo "I am here because you want to be Updated"
+echo[
+echo Press 1=="World News"
+echo Press 2=="Tech News"
+echo Press 3=="Stock News"
 
+
+   :ConfirmBox 
+        set /P c= Which News you want to be updated with (1/2/3) ?  
+
+    if /I "%c%" EQU "1" (
+    goto :WorldNews
+    ) else if /I "%c%" EQU "2" ( 
+    goto :TechNews
+    )  else if /I "%c%" EQU "3" ( 
+    goto :StockNews
+    ) else ( 
+    goto :InValid 
+    )
+
+
+:WorldNews
+    echo I am here because you want to see World News
+    echo "I am here because you want to see News"
+	start "Brave" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" https://www.bizshala.com/ https://merolagani.com/ https://www.cnbc.com/world/ https://www.setopati.com/ https://www.onlinekhabar.com/ https://www.ratopati.com/ https://ekantipur.com/ https://www.bloomberg.com/asia https://www.cnbc.com/world/ https://t.me/WorldNews https://www.theguardian.com/international https://www.businessinsider.com/sai https://www.vice.com/en https://www.thehimalayantimes.com/ https://www.hamropatro.com/news https://annapurnapost.com/ https://www.bbc.com/news/world https://nepsealpha.com/ https://www.sciencealert.com/ https://www.insider.com/asia? https://myrepublica.nagariknetwork.com/news/nepse-up-marginally-after-sunday-s-rally/ https://news.google.com/topstories/
+	goto:END
+
+rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
+
+
+:TechNews
+    echo "I am here because you want to see Tech News"
+	start "Brave" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"  https://news.ycombinator.com/ https://hackersonlineclub.com/ https://www.theverge.com/ https://www.xda-developers.com/ https://www.bleepingcomputer.com/ https://www.maketecheasier.com/ https://www.pcmag.com/ https://www.businessinsider.com/sai https://www.androidpolice.com/ https://www.windowscentral.com/blog https://www.computerworld.com/in/category/windows/ https://www.linuxtoday.com/ https://thehackernews.com/ https://www.androidauthority.com/ https://www.gadgetbytenepal.com/ https://mspoweruser.com/ https://beebom.com/ https://t.me/Science_News_Facts_Updates_daily https://t.me/Technology_updates https://www.makeuseof.com/  https://www.techpana.com/ 
+    goto :END 
+
+rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
+
+:StockNews
+    echo "I am here because you want to see Stock News"
+	start "Brave" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"  http://www.nepalstock.com/news/category/0 https://merolagani.com/ https://nepsealpha.com/ https://www.bizshala.com/ https://newweb.nepalstock.com.np/ https://www.sharebazarnepal.com.np/ http://nepalstockinfo.com/ https://investingnepal.com/ https://www.sharesansar.com/ https://www.nepalipaisa.com/ https://merocapital.com/
+	goto: END
+
+rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
  
-#pause
+rem pause
+
 exit
 
 :somewhere_else
 
-echo "I am here because you want to see Tech News"
-start "Firefox" "C:\Program Files\Mozilla Firefox\firefox.exe" https://www.theverge.com/ https://www.xda-developers.com/ https://www.bleepingcomputer.com/ https://www.maketecheasier.com/ https://www.pcmag.com/ https://www.businessinsider.com/sai https://www.androidpolice.com/ https://www.windowscentral.com/blog https://www.computerworld.com/in/category/windows/ https://www.linuxtoday.com/ https://thehackernews.com/ https://www.androidauthority.com/ https://www.gadgetbytenepal.com/ https://mspoweruser.com/ https://beebom.com/ https://www.kitploit.com/ https://t.me/Science_News_Facts_Updates_daily https://t.me/Technology_updates 
-#start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
-#pause
+echo "I am here because it's Business hrs"
+start "Chrome" "C:\Program Files\Google\Chrome\Application\chrome.exe" https://tms51.nepsetms.com.np/login http://www.nepalstock.com/news/category/0 https://meroshare.cdsc.com.np/#/login https://newweb.nepalstock.com.np/ https://nepsealpha.com/trading/chart
+%SystemRoot%\explorer.exe "D:\Power\DEca.xlsx"
+
+
+rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
+rem pause
 exit
 
 
 :faraway
 
-echo "I am here because you want to see Stock "
-start "Firefox" "C:\Program Files\Mozilla Firefox\firefox.exe" https://tms51.nepsetms.com.np/login http://www.nepalstock.com/news/category/0 https://meroshare.cdsc.com.np/#/login https://merolagani.com/ https://newweb.nepalstock.com.np/ https://www.sharebazarnepal.com.np/ http://nepalstockinfo.com/ https://investingnepal.com/ https://www.bizshala.com/ http://www.mexnepal.com/webpages/technical_analysis_mex.html https://merocapital.com/company/nepse/nepal-stock-exchange-limited/advanced_technical_chart/ https://nepsealpha.com/trading/chart https://www.sharesansar.com/ https://www.nepalipaisa.com/
-%SystemRoot%\explorer.exe "D:\DEca.xlsx"
+echo "I am here because you want to see Your Project Folder "
+%SystemRoot%\explorer.exe "D:\asd"
 
-#pause
+rem pause
 exit
 
 :bitfar
 echo "I am here because you want to travel to folders "
 echo[
-echo Press 1=="fFile"
-echo Press 2=="f File"
-echo Press 3=="file"
+echo Press 1=="assad File"
+echo Press 2=="assadd File"
+echo Press 3=="assadFile"
 
 
    :ConfirmBox 
@@ -76,16 +119,16 @@ echo Press 3=="file"
 
 
 :Fnpy
-    echo Travelling to Python Folder
-    %SystemRoot%\explorer.exe "D:\Aozlrkasteck"
+    echo Travelling to Folder
+    %SystemRoot%\explorer.exe "D:\"
     goto :END
 :Fncmd
     echo Travelling to Command Folder
-    %SystemRoot%\explorer.exe "D:\Aozlrkasteckn"
+    %SystemRoot%\explorer.exe "D:\"
     goto :END
 :Fnexcel
     echo Travelling to Excel Folder
-    %SystemRoot%\explorer.exe "D:\Aozlrkasteck"
+    %SystemRoot%\explorer.exe "D:\"
     goto :END
 :InValid
     echo Invalid selection. Enter given seleted number [1/2/3].
