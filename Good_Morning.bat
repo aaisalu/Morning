28da1337b508b793df44028785c220d69a0057d2
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-07-29 11:09:08 +0545

    firefox: Add firefox browser

--- Good_Morning.bat
+++ Good_Morning.bat
@@ -14,15 +14,15 @@ echo Jst a little bit more ...
 echo[
 
 echo Press 4=="Travel to Folders"
-echo Press 5=="QR-Code"
+rem echo Press 5=="QR-Code"
 
 echo[
-set /P c=Feed me just Number so I can work for you!! [1-5] ?  
+set /P c=Feed me just Number so I can work for you!! [1-4] ?  
 if /I "%c%" EQU "1" goto :somewhere
 if /I "%c%" EQU "2" goto :somewhere_else
 if /I "%c%" EQU "3" goto :faraway
 if /I "%c%" EQU "4" goto :bitfar
-if /I "%c%" EQU "5" goto :jstfar
+rem if /I "%c%" EQU "5" goto :jstfar
 goto :choice
 
 
@@ -50,9 +50,9 @@ echo Press 3=="Stock News"
 
 
 :WorldNews
-    echo I am here because you want to see World News
+    echo Travelling to Python Folder
     echo "I am here because you want to see News"
-	start "Brave" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" https://www.bizshala.com/ https://merolagani.com/ https://www.cnbc.com/world/ https://www.setopati.com/ https://www.onlinekhabar.com/ https://www.ratopati.com/ https://ekantipur.com/ https://www.bloomberg.com/asia https://www.cnbc.com/world/ https://t.me/WorldNews https://www.theguardian.com/international https://www.businessinsider.com/sai https://www.vice.com/en https://www.thehimalayantimes.com/ https://www.hamropatro.com/news https://annapurnapost.com/ https://www.bbc.com/news/world https://nepsealpha.com/ https://www.sciencealert.com/ https://www.insider.com/asia? https://myrepublica.nagariknetwork.com/news/nepse-up-marginally-after-sunday-s-rally/ https://news.google.com/topstories/
+	start "Firefox" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://www.bizshala.com/ https://merolagani.com/ https://www.cnbc.com/world/ https://www.setopati.com/ https://www.onlinekhabar.com/ https://www.ratopati.com/ https://ekantipur.com/ https://www.bloomberg.com/asia https://www.cnbc.com/world/ https://t.me/WorldNews https://www.theguardian.com/international https://www.businessinsider.com/sai https://www.vice.com/en https://www.thehimalayantimes.com/ https://www.hamropatro.com/news https://annapurnapost.com/ https://www.bbc.com/news/world https://nepsealpha.com/ https://www.sciencealert.com/ https://www.insider.com/asia? https://myrepublica.nagariknetwork.com/news/nepse-up-marginally-after-sunday-s-rally/ https://news.google.com/topstories/
 	goto:END
 
 rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
@@ -60,14 +60,14 @@ rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbw
 
 :TechNews
     echo "I am here because you want to see Tech News"
-	start "Brave" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"  https://news.ycombinator.com/ https://hackersonlineclub.com/ https://www.theverge.com/ https://www.xda-developers.com/ https://www.bleepingcomputer.com/ https://www.maketecheasier.com/ https://www.pcmag.com/ https://www.businessinsider.com/sai https://www.androidpolice.com/ https://www.windowscentral.com/blog https://www.computerworld.com/in/category/windows/ https://www.linuxtoday.com/ https://thehackernews.com/ https://www.androidauthority.com/ https://www.gadgetbytenepal.com/ https://mspoweruser.com/ https://beebom.com/ https://t.me/Science_News_Facts_Updates_daily https://t.me/Technology_updates https://www.makeuseof.com/  https://www.techpana.com/ 
+	start "Firefox" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://news.ycombinator.com/ https://hackersonlineclub.com/ https://www.theverge.com/ https://www.xda-developers.com/ https://www.bleepingcomputer.com/ https://www.maketecheasier.com/ https://www.pcmag.com/ https://www.businessinsider.com/sai https://www.androidpolice.com/ https://www.windowscentral.com/blog https://www.computerworld.com/in/category/windows/ https://www.linuxtoday.com/ https://thehackernews.com/ https://www.androidauthority.com/ https://www.gadgetbytenepal.com/ https://mspoweruser.com/ https://beebom.com/ https://t.me/Science_News_Facts_Updates_daily https://t.me/Technology_updates https://www.makeuseof.com/  https://www.techpana.com/ 
     goto :END 
 
 rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
 
 :StockNews
     echo "I am here because you want to see Stock News"
-	start "Brave" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"  http://www.nepalstock.com/news/category/0 https://merolagani.com/ https://nepsealpha.com/ https://www.bizshala.com/ https://newweb.nepalstock.com.np/ https://www.sharebazarnepal.com.np/ http://nepalstockinfo.com/ https://investingnepal.com/ https://www.sharesansar.com/ https://www.nepalipaisa.com/ https://merocapital.com/
+	start "Firefox" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" http://www.nepalstock.com/news/category/0 https://merolagani.com/ https://nepsealpha.com/ https://www.bizshala.com/ https://newweb.nepalstock.com.np/ https://www.sharebazarnepal.com.np/ http://nepalstockinfo.com/ https://investingnepal.com/ https://www.sharesansar.com/ https://www.nepalipaisa.com/ https://merocapital.com/
 	goto: END
 
 rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
@@ -79,8 +79,8 @@ exit
 :somewhere_else
 
 echo "I am here because it's Business hrs"
-start "Chrome" "C:\Program Files\Google\Chrome\Application\chrome.exe" https://tms51.nepsetms.com.np/login http://www.nepalstock.com/news/category/0 https://meroshare.cdsc.com.np/#/login https://newweb.nepalstock.com.np/ https://nepsealpha.com/trading/chart
-%SystemRoot%\explorer.exe "D:\Power\DEca.xlsx"
+start "Chrome" "C:\Program Files\Mozilla Firefox\firefox.exe" https://tms51.nepsetms.com.np/login http://www.nepalstock.com/news/category/0 https://meroshare.cdsc.com.np/#/login https://newweb.nepalstock.com.np/ https://nepsealpha.com/trading/chart
+%SystemRoot%\explorer.exe "D:\power"
 
 
 rem start /min "" explorer.exe shell:AppsFolder\Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
@@ -91,7 +91,7 @@ exit
 :faraway
 
 echo "I am here because you want to see Your Project Folder "
-%SystemRoot%\explorer.exe "D:\asd"
+%SystemRoot%\explorer.exe "D:\apple"
 
 rem pause
 exit
@@ -99,9 +99,9 @@ exit
 :bitfar
 echo "I am here because you want to travel to folders "
 echo[
-echo Press 1=="assad File"
-echo Press 2=="assadd File"
-echo Press 3=="assadFile"
+echo Press 1 ==" File"
+echo Press 2 ==" File"
+echo Press 3 ==" File"
 
 
    :ConfirmBox 
@@ -119,65 +119,66 @@ echo Press 3=="assadFile"
 
 
 :Fnpy
-    echo Travelling to Folder
-    %SystemRoot%\explorer.exe "D:\"
+    echo Travelling to  Folder
+    %SystemRoot%\explorer.exe "D:\apple"
     goto :END
 :Fncmd
     echo Travelling to Command Folder
-    %SystemRoot%\explorer.exe "D:\"
+    %SystemRoot%\explorer.  "D:\apple""
     goto :END
 :Fnexcel
-    echo Travelling to Excel Folder
-    %SystemRoot%\explorer.exe "D:\"
-    goto :END
+    echo Travelling to  Folder
+    %SystemRoot%\explorer.exe  "D:\apple"
+
+    goto :ENDWeb
 :InValid
     echo Invalid selection. Enter given seleted number [1/2/3].
     goto :ConfirmBox
 
 
-:jstfar 
-	@echo off
+rem :jstfar 
+rem 	@echo off
 	
-	echo[	
-	@echo off
-	md %USERPROFILE%\pictures\QRcode   
-	pip install qrcode[pil]		
+rem 	echo[	
+rem 	@echo off
+rem 	md %USERPROFILE%\pictures\QRcode   
+rem 	pip install qrcode[pil]		
 
 
-	set Output="%USERPROFILE%\pictures\QRcode"
-	set File="%USERPROFILE%\pictures\QRcode"
+rem 	set Output="%USERPROFILE%\pictures\QRcode"
+rem 	set File="%USERPROFILE%\pictures\QRcode"
 	
-	echo[	
+rem 	echo[	
 	
-	echo Press 1=="Create QR code"
-	echo Press 2=="NO QR code"
+rem 	echo Press 1=="Create QR code"
+rem 	echo Press 2=="NO QR code"
 
-   :Box 
-        set /P c= Create QR to share Fast (1/2) ? 
-	echo[
-	if /I "%c%" EQU "1" (
-	goto :Yesqr 
-    	) else if /I "%c%" EQU "2" ( 
-    	goto :Noqr)
-	goto:Box
+rem    :Box 
+rem         set /P c= Create QR to share Fast (1/2) ? 
+rem 	echo[
+rem 	if /I "%c%" EQU "1" (
+rem 	goto :Yesqr 
+rem     	) else if /I "%c%" EQU "2" ( 
+rem     	goto :Noqr)
+rem 	goto:Box
 
 
-:Yesqr
-	set /p Qrask=Type desired text to convert into Qr code:
-	REM enter your desired output here
-	qr %Qrask% >> %Output%\Qr.png
-	echo Qrinput   = %Qrask% >> %Output%\Qrinputdata.txt
-	echo File has been placed %File%	
-	pause	
-	exit
+rem :Yesqr
+rem 	set /p Qrask=Type desired text to convert into Qr code:
+rem 	REM enter your desired output here
+rem 	qr %Qrask% >> %Output%\Qr.png
+rem 	echo Qrinput   = %Qrask% >> %Output%\Qrinputdata.txt
+rem 	echo File has been placed %File%	
+rem 	pause	
+rem 	exit
 	
 	
 
 	
-:Noqr	
-	echo No Jokearound here!!
-	timeout 2 >nul
-	goto :END
+rem :Noqr	
+rem 	echo No Jokearound here!!
+rem 	timeout 2 >nul
+rem 	goto :END
 
 pause
 exit
