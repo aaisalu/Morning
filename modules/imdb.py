7e25f42a4711fb19d1eaa2b8626e682a69f5039c
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-07-30 16:00:12 +0545

    Imdb: Optimized & Cleaned - Fixed lots of smol bugs - Better at Scraping with less error - Handles error like never before - Feature to show score, rating box etc

--- modules/imdb.py
+++ modules/imdb.py
@@ -2,36 +2,26 @@ from bs4 import BeautifulSoup
 import requests,csv
 
 url=input("Please feed the imdb website: ")
-def imdb_v1(url):		
-	pop=requests.get(url).text
-	Soup=BeautifulSoup(pop,'lxml')
-	ids=Soup.find_all('div',class_="lister-item mode-advanced")
-	for box in ids:
-		Title=box.h3.a.text
-		Year=(box.find('span',class_="lister-item-year text-muted unbold")).text
-		rating=(box.find('div',class_='inline-block ratings-imdb-rating')).strong.text
-		votes=(box.find('p',class_='sort-num_votes-visible')).find_all('span')[1].text
-		# NOTE:
-		# YOU CA find the find_all by adding {} like a dictionary and get the data
-		# if there is dulicate vakues like {'name':'nv'} then you can use {} dictionary method
-		# to slice the data .SLice the data accoring to it.
-
-		# Remember find_ALL is depreciated in bs4 instead use find_all
-
-		# like in Money code there is no Gross in first index but there is in second index.
-		# so overcome error we convert into len()>1 if found gives tthe bank[1]
-
-		bank=box.find_all('span',{'name':'nv'})
-		Money=bank[1].text if len(bank)>1 else 'No any, Data Found'
-		
-		summary=(box.find_all('p',class_='text-muted')[-1]).text.strip()
-
-		# Time=(box.find('p').text).replace('|',' ').replace(' ','').strip()
-		
-		Time=(box.find('p',class_='text-muted')).find('span',class_='runtime').text
-		types=(((box.find('p',class_='text-muted').find('span',class_='genre'))).text).strip()
-		with open('Imdb_v1.csv',newline='',mode='a+') as fire:
-			spam=csv.writer(fire,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-			spam.writerow([Title,summary,types,Time,Money,Year,rating,votes])
-
-imdb_v1()
+def imdb_v2(url):
+	i=0
+	pop=BeautifulSoup(requests.get(url).text,"lxml")
+	Garage=pop.find_all('div',class_='lister-item mode-advanced')
+	for Container in Garage:
+		i+=1
+		Title=Container.find('h3',{"class":'lister-item-header'}).a.text
+		Year=Container.find('span',{'class':'lister-item-year text-muted unbold'}).text
+		Genre=Container.find('span',class_='genre').text.strip()
+		Time=Container.find('span',class_='runtime').text
+		Certificate="Error Fetching Data" if Container.find('span',class_='certificate')==None else Container.find('span',class_='certificate').text 
+		Ratebox=Container.find('div',class_='ratings-bar')
+		Rate=Ratebox.find('span',class_='rating-rating').text
+		Score="Data Not Found on Site " if Ratebox.find('span',class_='metascore favorable')==None else Ratebox.find('span',class_='metascore favorable').text
+		Paragraph=Container.find_all('p',class_='text-muted')[1].text.strip()
+		Votes=Container.find('p',class_='sort-num_votes-visible').find('span',{'name':'nv'}).text	
+		Bank=Container.find_all('span',{'name':'nv'})
+		Gross=Bank[1].text if len(Bank)>1 else "No any Data Found on Site "
+		with open('Imdb_v2.csv' ,'a+') as k:
+			pop=csv.writer(k,delimiter=',')
+			pop.writerow([i,Title,Year,Genre,Time,Certificate,Rate,Score,Paragraph,Votes,Gross])
+	
+imdb_v2(url)
