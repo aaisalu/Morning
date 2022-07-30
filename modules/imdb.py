5450be01aa2efeb37b90fa4ddeee9d59d23c714e
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-07-30 16:32:30 +0545

    IMdb: Stable as gimble - Crawl data like spider outputs like thor hammer - Minimized code without dropping performance - Fixed bugs which outputs gross at top250 - Fixed lots of bugs
    Straight to Stable release from alpha release
    will implement threading on next build :)

--- modules/imdb.py
+++ modules/imdb.py
@@ -3,37 +3,35 @@ import requests,csv
 
 url=input("Please feed the imdb website: ")
 
-def imdb_v4(url):
-	# url='https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=100&start=%27+str(hi)+%27&ref_=adv_nxt'
-	# url='https://www.imdb.com/search/title/?count=100&groups=oscar_best_picture_winners&sort=year%2Cdesc&ref_=nv_ch_osc'
-	incoming=requests.get(url).text
-	parse=BeautifulSoup(incoming,'lxml')
-	storage=parse.find_all('div',class_='lister-item mode-advanced')
-	for data in storage:
-		index=data.find('span','lister-item-index unbold text-primary').text
-		title=data.h3.a.text
-		year=data.h3.find('span',class_='lister-item-year text-muted unbold').text.replace('(','').replace(')','')
 
-		ratings=data.find('div',class_='ratings-bar')
-		rate=f"{ratings.strong.text}/10"
-		meta_box=ratings.find('div',class_='inline-block ratings-metascore')
-		meta=meta_box.span.text.strip() if meta_box else "Oh ! my Meta_score"
-		
-		Divs=data.find_all('p','text-muted')
-		certify= cert.text if (cert:=(Divs[0].find('span',{"class":"certificate"}))) else "Certificate seems to be lost"
-		time=Divs[0].find('span',{"class":"runtime"}).text.strip()
-		genre=Divs[0].find('span',{"class":"genre"}).text.strip()
+def imdb_v5(url):
+	# url='https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=100&start=%27+str(hi)+%27&ref_=adv_nxt'
+	incoming=requests.get(url).text
+	soup=BeautifulSoup(incoming,'lxml')
+	data=soup.find_all('div',{"class":'lister-item mode-advanced'})
+	for index,box in enumerate(data):
+
+		title=box.h3.a.text
+		year=box.find('span',class_='lister-item-year text-muted unbold').text.replace('(','').replace(')','')
+		rating=box.find('div','ratings-bar')
+		rate=f'{rating.strong.text}/10'
+		meta=star.span.text if (star:=rating.find('div',class_='inline-block ratings-metascore')) else "Oh ! my Meta_score"
+
+		Divs=box.find_all('p',{'class':'text-muted'})
+		certificate=cert.text.strip() if (cert:=Divs[0].find('span',class_='certificate')) else "Certificate seems to be lost"
+		time=clock.text if (clock:=Divs[0].find('span',class_='runtime')) else "Time Seems to be unlimited?"
+		genre=dress.text.strip() if (dress:=Divs[0].find('span',class_='genre')) else "Genre seems to be flying"
 		summary=Divs[1].text.strip()
 
-		vote=data.find_all("span",{"name":"nv"})[0].text
-		gross_check=growth[1].text if len(growth:=(data.find_all("span",{"name":"nv"})))>1   else "Gross seems to be hidden"
-		gross=gross_check if "#" not in gross_check else "Gross seems to be hidden"
-
-		top_250= top[-1].text if len(top:=(data.find_all("span",{"name":"nv"})))>2 else "Error on Top 250"
-
-		with open("imdb_v4.csv",mode='a+',newline='') as editor:
+		ballot =box.find_all('span',{'name':"nv"})
+		vote   =ballot[0].text
+		gross  =ballot[1].text if len(ballot)>2 and '$' in ballot[1].text and '#' not in ballot[1].text  else "Still Collecting Cash"
+		top_250=ballot[2].text if len(ballot)>2 and '#' in ballot[2].text and '$' not in ballot[2].text else "Can't be found in Top 250"
+		
+		with open("imdb_v5.csv",mode='a+',newline='') as editor:
 			write=csv.writer(editor,delimiter=',')
-			write.writerow([index,title,rate,year,time,genre,summary,vote,meta,gross,certify,top_250])
+			write.writerow([index,title,rate,year,time,genre,summary,vote,meta,gross,certificate,top_250])
 		
 
-imdb_v4(url)
+imdb_v5(url)
+
