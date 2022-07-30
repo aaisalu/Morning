1f33f9952c02ecee699f768013d6ce66a1cad741
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-07-30 16:13:58 +0545

    Imdb: Clean as spring water - Better readable code with reuseable codes - Added feature to scrap Top_250 - Fixed the duration time which shows incorrect data - Better error handling while scraping Gross column

--- modules/imdb.py
+++ modules/imdb.py
@@ -2,36 +2,38 @@ from bs4 import BeautifulSoup
 import requests,csv
 
 url=input("Please feed the imdb website: ")
-def imdb_v3(url):
+
+def imdb_v4(url):
 	# url='https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=100&start=%27+str(hi)+%27&ref_=adv_nxt'
-	info=requests.get(url).text
-	soup=BeautifulSoup(info,'lxml')
-	library=soup.find_all('div',class_='lister-item mode-advanced')
-	for smol in library:
-		index=smol.find('h3',class_='lister-item-header').span.text
-		title=smol.find('h3',class_='lister-item-header').a.text
-		year=smol.find('h3',class_='lister-item-header').find('span',{'class':"lister-item-year text-muted unbold"}).text.replace('(','').replace(')','')## OR [1:5]
-		rating=f"{smol.find('div',class_='ratings-bar').strong.text}/10"
-		votes=smol.find_all('p',class_='sort-num_votes-visible')[0].find('span',{'name':"nv"}).text
+	# url='https://www.imdb.com/search/title/?count=100&groups=oscar_best_picture_winners&sort=year%2Cdesc&ref_=nv_ch_osc'
+	incoming=requests.get(url).text
+	parse=BeautifulSoup(incoming,'lxml')
+	storage=parse.find_all('div',class_='lister-item mode-advanced')
+	for data in storage:
+		index=data.find('span','lister-item-index unbold text-primary').text
+		title=data.h3.a.text
+		year=data.h3.find('span',class_='lister-item-year text-muted unbold').text.replace('(','').replace(')','')
 
-		Div_box=smol.find_all('p',class_='text-muted')
-		time=Div_box[0].find(class_='runtime').text
-		genre=Div_box[0].find(class_='genre').text.strip()
-		summary=Div_box[1].text.strip()
+		ratings=data.find('div',class_='ratings-bar')
+		rate=f"{ratings.strong.text}/10"
+		meta_box=ratings.find('div',class_='inline-block ratings-metascore')
+		meta=meta_box.span.text.strip() if meta_box else "Oh ! my Meta_score"
+		
+		Divs=data.find_all('p','text-muted')
+		certify= cert.text if (cert:=(Divs[0].find('span',{"class":"certificate"}))) else "Certificate seems to be lost"
+		time=Divs[0].find('span',{"class":"runtime"}).text.strip()
+		genre=Divs[0].find('span',{"class":"genre"}).text.strip()
+		summary=Divs[1].text.strip()
 
-		Meta_box=smol.find('div',class_="ratings-bar").find('div',class_='inline-block ratings-metascore')
-		meta=Meta_box.span.text.strip() if Meta_box else "No Entry Found"
-		
-		Old_top=smol.find_all('span',{'name':'nv'})[-1].text
-		Top_box=smol.find_all('span',{'name':"nv"})[-1].text
-		top_250=Top_box.replace('#','') if '#' in Top_box else "Can't find in Top 250"
-		
-		Gross_box=smol.find_all('span',{'name':'nv'})
-		Check_point=Gross_box[1].text if len(Gross_box)>1  else "No Entry Found"
-		gross=Check_point if '#' not in Check_point else "No Entry Found"
-		with open('Imdb_v3.csv',newline='',mode='a+') as hello:
-			pop=csv.writer(hello,delimiter=',')
-			pop.writerow([index,title,rating,year,time,genre,summary,votes,meta,top_250,gross])
+		vote=data.find_all("span",{"name":"nv"})[0].text
+		gross_check=growth[1].text if len(growth:=(data.find_all("span",{"name":"nv"})))>1   else "Gross seems to be hidden"
+		gross=gross_check if "#" not in gross_check else "Gross seems to be hidden"
+
+		top_250= top[-1].text if len(top:=(data.find_all("span",{"name":"nv"})))>2 else "Error on Top 250"
+
+		with open("imdb_v4.csv",mode='a+',newline='') as editor:
+			write=csv.writer(editor,delimiter=',')
+			write.writerow([index,title,rate,year,time,genre,summary,vote,meta,gross,certify,top_250])
 		
 
-imdb_v3(url)
+imdb_v4(url)
