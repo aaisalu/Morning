f535657b1535d72bb0cab24b74445fffc36f5131
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-07-30 09:59:44 +0545

    youtube:Fix progress bar - Every unique download its own progress bar - Added line break for better visibility

--- modules/youtube.py
+++ modules/youtube.py
@@ -16,37 +16,36 @@ def folder(file):
 
 def mp3(url):
     yt=pytube.YouTube(url)
-    print(":)")
     print(f"Title: {yt.title} ")
     print(f"Views: {yt.views} Duration:{yt.length}")
-    print("")
     try: 
         out_file = pytube.YouTube(url,on_progress_callback=on_progress).streams.filter(only_audio=True).first().download(folder("Audios"))
+        print(":) \n")        
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
         os.rename(out_file, new_file)
-    except FileExistsError as file:
-        print(f"Looks like {yt.title} Music is already present")
+
+    except FileExistsError:
+        print(f"Looks like {yt.title} Music is already present. \n")
 
 
 def solo_video(url):
     yt=pytube.YouTube(url)
-    print(":)")
     print(f"Title: {yt.title} ")
-    print(f"Views: {yt.views} Duration: {yt.length}")
-    print("")
+    print(f"Views: {yt.views} Duration: {yt.length}") 
     pytube.YouTube(url,on_progress_callback=on_progress).streams.get_highest_resolution().download(folder("Videos"))     
+    print(":) \n")
 
 def playlists(link,ask):
     urls=[]
     playlist = Playlist(link)
     print('Number of videos in playlist: %s' % len(playlist.video_urls))
-    if re.search("audio|mp3|music|flac|wav|aac|ogg",ask,flags=re.IGNORECASE):
-        print("Starting to download MP3s of the videos")
+    if re.search("audio|mp3|music|flac|wav|aac|ogg|audios",ask,flags=re.IGNORECASE):
+        print("Starting to download MP3s of the videos \n")
         for music_url in playlist.video_urls:
             mp3(music_url)
     else:
-        print("Starting to download videos")
+        print("Starting to download videos \n")
         for video_url in playlist.video_urls:
             solo_video(video_url)     
 
@@ -55,11 +54,11 @@ def checkbox(link):
     if "list" in link:
         playlists(link,ask)      
     else:
-        if re.search("audio|mp3|music|flac|wav|aac|ogg",ask,flags=re.IGNORECASE):
-            print("Starting to download MP3s of the video")
+        if re.search("audio|mp3|music|flac|wav|aac|ogg|audios",ask,flags=re.IGNORECASE):
+            print("Starting to download MP3s of the video \n")
             mp3(link)
         else:
-            print("Starting to download video")
+            print("Starting to download video \n")
             solo_video(link)
 
 url=checkbox(str(input("Enter the url of the video: ")))               
