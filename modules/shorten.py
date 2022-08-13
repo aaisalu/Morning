c03f5dc8c5d7552370cd28822ca2027ff3e05b90
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-08-13 08:59:27 +0545

    shorten: Add exception handling - Raise error in readable way - Add feature to support multiple links - Minor bug fixes

--- modules/shorten.py
+++ modules/shorten.py
@@ -1,10 +1,29 @@
 import bitlyshortener
 from configparser import ConfigParser
 
-config = ConfigParser()
-config.read('config.cfg')
-get_tokens = [config['API']['Bitly_token']]
-shortener = bitlyshortener.Shortener(tokens=get_tokens, max_cache_size=256)
 
-ask = str(input('Enter the url: '))
-print(shortener.shorten_urls([ask]))
+def get_token():
+    tokens = []
+    config = ConfigParser()
+    config.read('config.cfg')
+    data = config['API']['Bitly_token']
+    if data:
+        tokens.append(data.strip())
+        return tokens
+    return None
+
+
+def process_it(url):
+    if (api := (get_token())):
+        try:
+            shortener = bitlyshortener.Shortener(
+                tokens=api, max_cache_size=256)
+            for shorten in shortener.shorten_urls(url):
+                print(f'Your shorten link is {shorten}')
+        except bitlyshortener.exc.RequestError:
+            print("Looks like your input or the token is wrong")
+    else:
+        print("Please provide the token")
+
+
+process_it(input("Enter the url: ").split(" "))
