f491d34d73985a549ad9f294e8fe4d4494e58afe
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-08-17 20:04:12 +0545

    shorten: add except when api is not present

--- modules/shorten.py
+++ modules/shorten.py
@@ -3,34 +3,51 @@ from configparser import ConfigParser
 from termcolor import cprint
 import colorama
 import pyperclip
+import sys
 colorama.init()
 
 
 def get_token():
-    tokens = []
-    config = ConfigParser()
-    config.read('../config.ini')
-    data = config['API']['Bitly_token']
-    if data:
-        tokens.append(data.strip())
-        return tokens
-    return None
+    try:
+        tokens = []
+        config = ConfigParser()
+        config.read('../config.ini')
+        data = config['API']['Bitly_token']
+        if data:
+            tokens.append(data.strip())
+            return tokens
+        return None
+    except KeyError:
+        cprint("Please provide the token!", 'red')
 
 
 def process_it(url):
-    if (api := (get_token())):
-        try:
-            shortener = bitlyshortener.Shortener(
-                tokens=api, max_cache_size=256)
-            for shorten in shortener.shorten_urls(url):
-                pyperclip.copy(shorten)
-                cprint(
-                    f"Your shorten link is {shorten} and it's copied to clipboard just paste it !", 'green')
-        except bitlyshortener.exc.RequestError:
+    api = get_token()
+    try:
+        shortener = bitlyshortener.Shortener(
+            tokens=api, max_cache_size=256)
+        for shorten in shortener.shorten_urls(url):
+            pyperclip.copy(shorten)
             cprint(
-                "Read the following error and act according to it.\n\nError: It might be one of these following error. \n 1. Invalid given URL \n 2. Invalid token\n 3. Network issue", 'red')
-    else:
-        cprint("Please provide the token", 'red')
+                f"Your shorten link is {shorten} and it's copied to clipboard just paste it !", 'green')
+    except bitlyshortener.exc.RequestError:
+        cprint(
+            "Read the following error and act according to it.\n\nError: It might be one of these following error. \n 1. Invalid given URL \n 2. Invalid token\n 3. Network issue", 'red')
+        sys.exit(1)
+
+    except bitlyshortener.exc.ArgsError:
+        cprint(f"Token looks fishy...check your token or API", 'red')
+        sys.exit(1)
 
 
-process_it(input("Enter the url: ").split(" "))
+def main():
+    try:
+        process_it(input("Enter the url: ").split(" "))
+    except KeyboardInterrupt:
+        print("Exiting from the script....")
+        sys.exit(1)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
