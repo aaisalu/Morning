1dc3e65f8691956b07af163a776f928a121d0270
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-09-17 13:48:06 +0545

    libgen: add advanced mode searching feature

--- modules/libgen.py
+++ modules/libgen.py
@@ -1,9 +1,9 @@
 from libgen_api.libgen_search import LibgenSearch
 from tabulate import tabulate
 import webbrowser
+import helper_func
 
-title = "Pride and Prejudice"
-
+get_rawdata = LibgenSearch()
 
 def html(data):
     htmls = f'''
@@ -28,20 +28,20 @@ def html(data):
 def process_it(chunk):
     body = []
     for bit in chunk:
+        resolved_download_links = get_rawdata.resolve_download_links(bit)
+        resolved_get = resolved_download_links['GET']
         unique_id = bit['ID']
+        Title = f"<a class='resolved_links' href={resolved_get} target='_blank'>{bit['Title']}</a>"
         Author = bit['Author']
-        Title = bit['Title']
         Publisher = bit['Publisher']
         Year = bit['Year']
         Pages = bit['Pages']
         Language = bit['Language']
         Size = bit['Size']
         Extension = bit['Extension']
-        resolved_download_links = t.resolve_download_links(bit)
-        resolved_get = resolved_download_links['GET']
         resolved_cloudfare = resolved_download_links['Cloudflare']
         resolved_ipfs = resolved_download_links['IPFS.io']
-        Donwload_links = f"<a class='resolved_links' href={resolved_get} target='_blank'>Link 1</a>&nbsp&nbsp<a class='resolved_links' href={resolved_cloudfare} target='_blank'>Link 2</a></br><a class='resolved_links' href={resolved_ipfs} target='_blank'>Link 3</a>"
+        Donwload_links = f"<a class='resolved_links' href={resolved_cloudfare} target='_blank'>Link 1</a></br><a class='resolved_links' href={resolved_ipfs} target='_blank'>Link 2</a>"
         Donwload_mirror = f"<a class='mirror_links' href={bit['Mirror_1']} target='_blank'>Mirror 1</a>&nbsp&nbsp<a class='mirror_links' href={bit['Mirror_2']} target='_blank'>Mirror 2</a></br><a class='mirror_links' href={bit['Mirror_3']} target='_blank'>Mirror 3</a>&nbsp&nbsp<a class='mirror_links' href={bit['Mirror_4']} target='_blank'>Mirror 4</a>"
         columns = unique_id, Title, Author, Publisher, Year, Pages, Language, Size, Extension, Donwload_links, Donwload_mirror
         body.append(columns)
@@ -51,7 +51,33 @@ def process_it(chunk):
     html(formatted)
 
 
-t = LibgenSearch()
-titles = t.search_title(title)
+def adv_mode(title_book):
+    book_filters=dict()
+    try:
+        year=input("In which year book was published ?: ")
+        if len(year)>=3:
+            book_filters.update({"Year": year})
+    except:
+        print('As the book pubished year is not well defined so, excluding it.')
+    try:
+        extension=input("Which file format would you like to choose? [pdf/epub]: ").lower()
+        if extension in ['pdf', 'ext']:
+            book_filters.update({"Extension": extension})
+    except:
+        print('Error selecting file format between pdf & epub so, excluding it.')
+    if book_filters:
+        exact_filter=True if helper_func.chkreg("",input("Do you want to enable exact match filers? [Yes/No]: ")) else False
+        adv_filter = get_rawdata.search_title_filtered(" Pride and Prejudice", book_filters, exact_match=exact_filter)
+        process_it(adv_filter)
+    else:
+       process_it(get_rawdata.search_title(title_book))
 
-process_it(titles)
+def ask_user():
+    title_book= input("Enter the book title: ")
+    if len(title_book)>=3:
+        return adv_mode(title_book) if helper_func.chkreg("",(input('Do you want to enable advance book search? [Yes/No]: '))) else process_it(get_rawdata.search_title(title_book))
+    print("Please enter at least 3 character book name!!")
+    return ask_user()
+
+
+ask_user()
