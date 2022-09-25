a5f3a3cafd8bfdf89a198a280dbb3dd683f31bee
Aaisalu <94603696+aaisalu@users.noreply.github.com>
2022-09-25 08:46:05 +0545

    libgen: remove width value

--- modules/libgen.py
+++ modules/libgen.py
@@ -14,7 +14,7 @@ get_rawdata = LibgenSearch()
 def html(data):
     htmls = f'''
 <!DOCTYPE html>
-<html lang="en" style="width:100%;">
+<html lang="en"">
 <head>
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
